bddb18e0d25be8ac8510bf2b554cb062ed529672
Andre Moeller <13366062+andregmoeller@users.noreply.github.com>
2018-12-25 21:33:40 +0100

    Makes the store picker change the URI

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,12 +1,20 @@
 import React from "react";
-import { gerFunName, getFunName } from "../helpers";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+    nameInput = React.createRef();
+
+    goToStore = event => {
+        event.preventDefault();
+        const name = this.nameInput.current.value;
+        this.props.history.push(`/store/${name}`);
+    };
+
     render() {
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore}>
                 <h2>Please Enter A Store</h2>
-                <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+                <input type="text" ref={this.nameInput} required placeholder="Store Name" defaultValue={getFunName()} />
                 <button type="submit">Visit Store →</button>
             </form>
         );
